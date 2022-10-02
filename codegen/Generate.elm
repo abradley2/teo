788f7661b895f22e1331b4c09c636aeaa2be0e37
Tony Bradley <tonybradley@tonys-mac-mini.myfiosgateway.com>
2022-10-02 13:25:42 -0400

    more codegen work

--- codegen/Generate.elm
+++ codegen/Generate.elm
@@ -1,32 +1,70 @@
-
 module Generate exposing (main)
 
 {-| -}
 
+import Dict exposing (Dict)
 import Elm
 import Elm.Annotation as Type
 import Gen.CodeGen.Generate as Generate
-import Gen.Helper
-import Json.Decode as Decode exposing (Value)
+import Gen.Dict
+import Json.Decode as Decode exposing (Decoder, Value)
+import Parser
+
 
 main : Program Value () ()
 main =
-    Generate.fromJson
-        [ file
-        ]
+    Generate.fromJson (Decode.map formatNode nodeDecoder) <|
+        \translationsDict ->
+            [ Elm.file
+                []
+                [ Elm.declaration "defaultTranslations" <|
+                    Dict.foldr
+                        (\k v genDict ->
+                            Gen.Dict.insert (Elm.string k) (Elm.string v) genDict
+                        )
+                        Gen.Dict.empty
+                        translationsDict
+                ]
+            ]
 
 
+type Node
+    = Translation String
+    | TranslationDict (Dict String Node)
 
-file : Elm.File
-file =
-    Elm.file [ "HelloWorld" ]
-        [ Elm.declaration "hello"
-            (Elm.string "World!")
 
-        -- Here's an example of using a helper file!
-        -- Add functions to codegen/helpers/{Whatever}.elm
-        -- run elm-codegen install
-        -- Then you can call those functions using import Gen.{Whatever}
-        , Elm.declaration "usingAHelper"
-            (Gen.Helper.add5 20)
+formatNode : Node -> Dict String String
+formatNode node =
+    let
+        runNode : String -> Node -> Dict String String -> Dict String String
+        runNode prefix n acc =
+            case n of
+                Translation str ->
+                    Dict.insert prefix str acc
+
+                TranslationDict dict ->
+                    Dict.foldl
+                        (\nextPrefix nextNode nextAcc ->
+                            runNode
+                                (if prefix == "" then
+                                    nextPrefix
+
+                                 else
+                                    prefix ++ "." ++ nextPrefix
+                                )
+                                nextNode
+                                nextAcc
+                        )
+                        Dict.empty
+                        dict
+                        |> Dict.union acc
+    in
+    runNode "" node Dict.empty
+
+
+nodeDecoder : Decoder Node
+nodeDecoder =
+    Decode.oneOf
+        [ Decode.map Translation Decode.string
+        , Decode.map TranslationDict (Decode.dict (Decode.lazy <| \_ -> nodeDecoder))
         ]
